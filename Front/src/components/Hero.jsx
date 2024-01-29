000f0286c7ee794579b3fa41ed0d4e76395b330a
Tarig Mekhazni <t.mekhazni@hotmail.fr>
2024-01-29 16:25:04 +0100

    hero main page finished

--- Front/src/components/Hero.jsx
+++ Front/src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
     axios.get(endpoints.popular).then((response) => {
       const movies = response.data.results;
       // const randomMovie = movies[Math.floor(Math.random() * movies.length)];
-      const randomMovie = movies[5];
+      const randomMovie = movies[0];
       setMovie(randomMovie);
     });
 
@@ -23,26 +23,32 @@ const Hero = () => {
   const { title, backdrop_path, release_date, overview } = movie;
 
   return (
-    <div className="">
-      <div className="relative ">
-        <div className="h-[100vh] w-full absolute opacity-30 bg-red "></div>
+    <div className="relative">
+      {/* layer with opacity  */}
+      <div className="absolute h-[80vh] w-full bg-black opacity-30 sm:h-full"></div>
+
+      {/* div with main picture */}
+      <div className="h-[80vh] sm:h-full ">
         <img
           src={`${endpoints.originalIMG}${backdrop_path}`}
           alt={title}
-          className="object-cover w-full aspect-video max-h-screen h-screen"
+          className="aspect-video h-full max-h-screen  w-full object-cover"
         />
-        <div className="absolute bottom-[30%] left-[5%] md:left-[2%] w-5/6 md:w-1/2 space-y-2">
-          <h2 className="text-2xl font-bold md:text-6xl">{title}</h2>
-          <p className="text-xl">{overview}</p>
-          {/* <p>{release_date}</p> */}
+        <div className="absolute bottom-[10%] left-[5%] w-5/6 space-y-2 md:bottom-[25%] md:left-[4%] md:w-1/2 ">
+          <h2 className="text-4xl font-bold md:text-6xl">{title}</h2>
+          <p className="line-clamp-6 text-sm md:line-clamp-none md:overflow-visible md:text-wrap ">
+            {overview}
+          </p>
+
           <div className="space-x-4">
-            <button className="px-6 py-2 rounded text-xl border text-black bg-gray-300 hover:brightness-150 ">
+            <button className="rounded bg-gray-300 px-6 py-2  text-xl text-black hover:brightness-150 ">
               Play
             </button>
-            <button className="px-6 py-2 rounded text-xl border bg-gray-500 opacity-50 hover:brightness-150">
+            <button className="rounded bg-gray-500 px-6 py-2  text-xl opacity-80 hover:brightness-150">
               More
             </button>
           </div>
+          <p className="text-sm text-gray-400 opacity-75">{release_date}</p>
         </div>
       </div>
     </div>
