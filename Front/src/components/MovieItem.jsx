991a67d496e786c44fdf5ff00c5a1a95e88630e6
Tarig Mekhazni <t.mekhazni@hotmail.fr>
2024-01-30 14:58:29 +0100

    start page sign up + form

--- Front/src/components/MovieItem.jsx
+++ Front/src/components/MovieItem.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { getImageUrl } from "../services/fetchSettings";
 
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+
 const MovieItem = ({ movie }) => {
+  // state for like
+  const [like, setLike] = useState(false);
+  // destructuring
   const { title, backdrop_path, poster_path } = movie;
 
   return (
-    <div className="relative m-2 inline-block w-[160px] cursor-pointer overflow-hidden rounded-lg sm:w-[200px] md:w-[240px] lg:w-[300px]">
+    <div className="relative m-2 inline-block h-60 w-[160px] cursor-pointer overflow-hidden rounded-lg sm:w-[200px] md:h-[200px] md:w-[240px] lg:w-[400px]">
       <img
-        className="block h-60 w-full object-cover object-top"
+        className="block h-full w-full object-cover object-top"
         src={getImageUrl(backdrop_path ?? poster_path, "w500")}
         alt={title}
       />
 
-      <div className="absolute left-0 top-0 h-60 w-full   ">
-        <p className="flex h-full w-full items-center justify-center whitespace-normal bg-black/80 text-xs font-bold text-white opacity-0 hover:opacity-100 md:text-lg text-center ">
+      <div className="absolute left-0 top-0 h-full w-full bg-black/80 opacity-0 hover:opacity-100">
+        <p className="0 flex h-full w-full items-center justify-center whitespace-normal text-center text-xs font-bold text-white md:text-lg ">
           {title}
+        </p>
+        <p>
+          {like ? (
+            <FaHeart
+              size={30}
+              className="absolute left-2 top-2  text-gray-300"
+            />
+          ) : (
+            <FaRegHeart
+              size={30}
+              className="absolute left-2 top-2 text-gray-300"
+            />
+          )}
         </p>
       </div>
     </div>
--- Front/src/pages/Signup.jsx
+++ Front/src/pages/Signup.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import netflixBackground from "../assets/pictures/Netflix Background.webp";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <>
+      <div className="h-screen w-full">
+        <img
+          className="hidden h-full w-full object-cover sm:block"
+          src={netflixBackground}
+          alt="netflix not logged in background"
+        />
+        <div className="fixed left-0 top-0 flex h-screen w-full  items-center justify-center bg-black/70">
+          <div className="fixed z-20 mt-20 h-full w-full bg-black/70  py-0 md:mt-0 md:h-[70vh] md:w-[50vw] md:max-w-[500px]">
+            <div className="h-full w-full flex-col px-[48px] py-[68px]">
+              <header>
+                <h1 className="mb-10 text-4xl">Sign In</h1>
+              </header>
+              <form className="flex w-full flex-col space-y-4" action="">
+                <input
+                  className="bg-gray-900/80 p-3"
+                  type="email"
+                  name=""
+                  id=""
+                  placeholder="Email"
+                />
+                <input
+                  className="bg-gray-900/80 p-3"
+                  type="password"
+                  name=""
+                  id=""
+                  placeholder="Password"
+                />
+              </form>
+              <footer className="">
+                <button className="mt-4 w-full bg-red-600 py-2">
+                  {" "}
+                  Sign In
+                </button>
+              </footer>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Signup;
