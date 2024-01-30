2ae7d0dfa4fb309eb9c7cb73e351384b32c69d84
Tarig Mekhazni <t.mekhazni@hotmail.fr>
2024-01-30 11:05:41 +0100

    swap mobile test swie ontouchstart + onTouchend, test sssion

--- Front/src/components/Hero.jsx
+++ Front/src/components/Hero.jsx
@@ -1,103 +1,12 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import endpoints, { getImageUrl } from "../services/fetchSettings";
-import leftChevron from "../assets/Icons/left-arrow.svg";
-import rightChevron from "../assets/Icons/right-arrow.svg";
+import Slider from "./Slider";
 
 const Hero = () => {
-  // initilization index 0
-  const [sliderIndex, setSliderIndex] = useState(0);
-
-  // fetch DB TMDB with index
-  const [movie, setMovie] = useState([]);
-
-  // using the index to select wich object
-  useEffect(() => {
-    // fetch data + select a random movie on the 20 results
-    axios.get(endpoints.popular).then((response) => {
-      const movies = response.data.results;
-      // const randomMovie = movies[Math.floor(Math.random() * movies.length)];
-      // const randomMovie = movies[sliderIndex];
-      setMovie(movies[sliderIndex]);
-    });
-
-    // add conditions if no data etc
-  }, [sliderIndex]);
-
-  // permet de changer l'index pour changer d'images
-  const toggleImage = (indexPayload) => {
-    // ici va utiliser une fonction call back directement dans l'élément qui change le state : "setSliderIndex". Pas svt utilisé
-    setSliderIndex((state) => {
-      if (indexPayload + state > 19) {
-        return 0;
-      } else if (indexPayload + state < 1) {
-        return 19;
-      } else {
-        return state + indexPayload;
-      }
-    });
-  };
-
-  // AUTOMATIQUE SLIDER
-  // useEffect(() => {
-  //   const intervalID = setInterval(() => toggleImage(+1), 5000);
-  //   return () => clearInterval(intervalID);
-  // }, []);
-
-  // Destructuring
-  const { title, backdrop_path, release_date, overview } = movie;
-
   return (
-    <div className="relative">
+    <div className="">
       {/* layer with opacity  */}
-      <div className="absolute h-[100vh] w-full bg-black opacity-30"></div>
+      <div className="absolute  h-[100vh] w-full bg-black opacity-30"></div>
 
-      {/* div with main picture */}
-      {movie && (
-        <div className="h-[100vh] ">
-          <div className="relative  h-full">
-            <img
-              src={`${getImageUrl(backdrop_path, "original")}`}
-              alt={title}
-              className="aspect-video h-full max-h-screen  w-full object-cover"
-            />
-            <button
-              onClick={() => toggleImage(-1)}
-              className="navigation-button prev-button absolute left-[100px] top-2/4 flex h-12 w-12 -translate-y-2/4 cursor-pointer items-center justify-center rounded-full border-2 bg-transparent hover:bg-[#4e4e4e56]"
-            >
-              <img
-                src={leftChevron}
-                className="w-4"
-                alt="left arrow previous "
-              />
-            </button>
-            <button
-              onClick={() => toggleImage(+1)}
-              className="navigation-button prev-button absolute right-[100px] top-2/4 flex h-12 w-12 -translate-y-2/4 cursor-pointer items-center justify-center rounded-full border-2 bg-transparent"
-            >
-              <img src={rightChevron} className="w-4" alt="rigth arrow next " />
-            </button>
-          </div>
-          <div className="sticky  bottom-[10%] left-[5%]  w-5/6 space-y-2 sm:w-[40%] md:left-[4%]">
-            <h2 className="text-4xl font-bold md:text-6xl">{title}</h2>
-            <p
-              className={` line-clamp-2 text-xs hover:line-clamp-none md:text-wrap`}
-            >
-              {overview}
-            </p>
-
-            <div className="space-x-4">
-              <button className="rounded bg-gray-300 px-6 py-2  text-xl text-black hover:brightness-150 ">
-                Play
-              </button>
-              <button className="rounded bg-gray-500 px-6 py-2  text-xl opacity-80 hover:brightness-150">
-                More
-              </button>
-            </div>
-            <p className="text-sm text-gray-400 opacity-75">{release_date}</p>
-          </div>
-        </div>
-      )}
+      <Slider />
     </div>
   );
 };
--- Front/src/components/Slider.jsx
+++ Front/src/components/Slider.jsx
@@ -1,80 +1,125 @@
-import React, { useEffect, useState } from "react";
-import sliderData from "../../data/sliderData";
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import endpoints, { getImageUrl } from "../services/fetchSettings";
 import leftChevron from "../assets/Icons/left-arrow.svg";
 import rightChevron from "../assets/Icons/right-arrow.svg";
 
-
 const Slider = () => {
-  // initilisation de l'index à 1
-  const [sliderIndex, setSliderIndex] = useState(1);
+  // initilization index 0
+  const [sliderIndex, setSliderIndex] = useState(0);
 
+  // fetch DB TMDB with index
+  const [movie, setMovie] = useState([]);
+
+  // using the index to select wich object
+  useEffect(() => {
+    // fetch data + select a random movie on the 20 results
+    axios.get(endpoints.popular).then((response) => {
+      const movies = response.data.results;
+      const randomMovie = movies[Math.floor(Math.random() * movies.length)];
+      //   const randomMovie = movies[sliderIndex];
+      setMovie(randomMovie);
+    });
+
+    // add conditions if no data etc
+  }, [sliderIndex]);
+
+  // Destructuring
+  const { title, backdrop_path, release_date, overview } = movie;
   // permet de changer l'index pour changer d'images
   const toggleImage = (indexPayload) => {
-    let newState;
-    //permet lorsqu'on arrive sur la photo 5 + payload +1 au click = 6 alors on repasse newState et donc sliderIndex à 1 pour revenir à la premiere photo
-    if (indexPayload + sliderIndex > sliderData.length) {
-      newState = 1;
-
-      // ici on veut revenir à 5 car on click sur le bouton précédent qui à un sliderindex à 1. Donc si - 1 serait à 0 = bug. On veut revenir à 5 qui est aussi égal à sliderData.length.
-    } else if (indexPayload + sliderIndex < 1) {
-      newState = sliderData.length;
-
-      // tous les autres cas de figure normaux
-    } else {
-      newState = indexPayload + sliderIndex;
-    }
-    setSliderIndex(newState);
-
-    //AUTOMATIQUE SLIDER
     // ici va utiliser une fonction call back directement dans l'élément qui change le state : "setSliderIndex". Pas svt utilisé
-    // setSliderIndex((state) => {
-    //   if (indexPayload + state > sliderData.length) {
-    //     return 1;
-    //   } else if (indexPayload + state < 1) {
-    //     return sliderData.length;
-    //   } else {
-    //     return state + indexPayload;
-    //   }
-    // });
+    setSliderIndex((state) => {
+      if (indexPayload + state > 19) {
+        return 0;
+      } else if (indexPayload + state < 1) {
+        return 19;
+      } else {
+        return state + indexPayload;
+      }
+    });
   };
 
   // AUTOMATIQUE SLIDER
   // useEffect(() => {
-  //   const intervalID = setInterval(() => toggleImage(+1), 2000);
-
+  //   const intervalID = setInterval(() => toggleImage(+1), 5000);
   //   return () => clearInterval(intervalID);
   // }, []);
 
+  // Gestion des événements tactiles
+  const touchStartX = useRef(0);
+  const touchEndX = useRef(0);
+
+  const handleTouchStart = (e) => {
+    touchStartX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchEnd = (e) => {
+    touchEndX.current = e.changedTouches[0].clientX;
+    handleSwipe();
+  };
+
+  const handleSwipe = () => {
+    const swipeDistance = touchStartX.current - touchEndX.current;
+    // Ajustez la distance de glissement selon votre besoin
+    if (swipeDistance > 50) {
+      toggleImage(+1); // Swipe vers la gauche
+    } else if (swipeDistance < -50) {
+      toggleImage(-1); // Swipe vers la droite
+    }
+  };
   return (
     <>
-      <p className="index-info mx-0 mb-[25px] mt-[115px] text-center   text-white ">
-        {/* SliderIndex commence à 1 avec le usestate, et la length correspond à "5" éléments dans le tableau de data */}
-        {sliderIndex} / {sliderData.length}
-      </p>
-      <div className="slider relative mx-auto my-0 aspect-[3/2] max-w-[600px]  ">
-        <p className="image-info z-1 absolute right-0 border-b-[1px] border-l-[1px] border-black bg-[#f1f1f1]  px-2 py-3">
-          {/* méthode qui va appeler un call back et retourne l'objet dont l'id est = à sliderIndex puis sort moi le resultat description */}
-          {sliderData.find((obj) => obj.id === sliderIndex).description}
-        </p>
-        <img
-          className="h-full w-full bg-cover"
-          //on importe les images 1 ou 2 ou 3 grace à l'index de sliderindex
-          src={`/images/img-${sliderIndex}.jpg`}
-          alt=""
-        />
-        <button
-          onClick={() => toggleImage(-1)}
-          className="navigation-button prev-button absolute -left-[100px] top-2/4 flex h-12 w-12 -translate-y-2/4 cursor-pointer items-center justify-center rounded-full border-2 bg-transparent hover:bg-[#4e4e4e56]"
+      {/* div with main picture */}
+      {movie && (
+        <div
+          className="h-[100vh] "
+          onTouchStart={handleTouchStart}
+          onTouchEnd={handleTouchEnd}
         >
-          <img src={leftChevron} className="w-4" alt="left arrow previous " />
-        </button>
-        <button
-          onClick={() => toggleImage(+1)}
-          className="navigation-button prev-button absolute -right-[100px] top-2/4 flex h-12 w-12 -translate-y-2/4 cursor-pointer items-center justify-center rounded-full border-2 bg-transparent"
-        >
-          <img src={rightChevron} className="w-4" alt="rigth arrow next " />
-        </button>
-      </div>
+          <div className=" h-full">
+            <img
+              src={`${getImageUrl(backdrop_path, "original")}`}
+              alt={title}
+              className="aspect-video h-full max-h-screen  w-full object-cover"
+            />
+            <button
+              onClick={() => toggleImage(-1)}
+              className="navigation-button prev-button absolute left-[5vw] top-2/4 flex h-12 w-12 -translate-y-2/4 cursor-pointer items-center justify-center rounded-full border-2 bg-transparent hover:bg-[#4e4e4e56]"
+            >
+              <img
+                src={leftChevron}
+                className="w-4"
+                alt="left arrow previous "
+              />
+            </button>
+            <button
+              onClick={() => toggleImage(+1)}
+              className="navigation-button prev-button absolute right-[5vw] top-2/4 flex h-12 w-12 -translate-y-2/4 cursor-pointer items-center justify-center rounded-full border-2 bg-transparent"
+            >
+              <img src={rightChevron} className="w-4" alt="rigth arrow next " />
+            </button>
+          </div>
+          <div className="sticky  bottom-[10%] left-[5%]  w-5/6 space-y-2 sm:w-[40%] md:left-[4%]">
+            <h2 className="text-4xl font-bold md:text-6xl">{title}</h2>
+            <p
+              className={` line-clamp-2 text-xs hover:line-clamp-none  md:text-wrap`}
+            >
+              {overview}
+            </p>
+
+            <div className="space-x-4">
+              <button className="rounded bg-gray-300 px-6 py-2  text-xl text-black hover:brightness-150 ">
+                Play
+              </button>
+              <button className="rounded bg-gray-500 px-6 py-2  text-xl opacity-80 hover:brightness-150">
+                More
+              </button>
+            </div>
+            <p className="text-sm text-gray-400 opacity-75">{release_date}</p>
+          </div>
+        </div>
+      )}
     </>
   );
 };
